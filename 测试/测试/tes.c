8c0b267556640e1c90ceadeccddc0aec57ba9d02
斯威 <1476656257@qq.com>
2020-09-26 19:31:15 +0800

    提交代码

---
+++ 测试/测试/tes.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//int main()
+//{
+//	char* p = "abc";
+//	char* q = "abc123";
+//	//while (*p = *q)
+//	//	printf("%c%c", *p, *q);
+//	printf("%p\n%p", p, q);
+//	return 0;
+//}
