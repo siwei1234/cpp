405b7eabb088fd90e849b375a83349753483f44f
斯威 <1476656257@qq.com>
2020-10-27 19:43:27 +0800

    提交代码

---
+++ mergeTwoLists/mergeTwoLists/test.cpp
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include<iostream>
+using namespace std;
+
+
+
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
+class Solution {
+public:
+	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+		ListNode* head = new ListNode(-1);
+		ListNode* cur = head;
+		while (l1 != NULL && l2 != NULL)
+		{
+			if (l1->val < l2->val)
+			{
+				cur->next = l1;
+				l1 = l1->next;
+			}
+			else
+			{
+				cur->next = l2;
+				l2 = l2->next;
+			}
+			cur = cur->next;
+		}
+		cur->next = l1 == NULL ? l2 : l1;
+		return head->next;
+	}
+};
+
+
+int main()
+{
+	return 0;
+}
